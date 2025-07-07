e1b15bda52f791ce03630958fb2f10e87dd711d2
luisangeluis <65631946+luisangeluis@users.noreply.github.com>
2025-07-07 21:05:42 +0000

    First commit

---
+++ FeliGavilanApiCourse/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using FeliGavilanApiCourse.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace FeliGavilanApiCourse.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Genre> Genres { get; set; }
+}
---
+++ FeliGavilanApiCourse/Entities/Product.cs
@@ -0,0 +1,7 @@
+﻿namespace FeliGavilanApiCourse.Entities;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+}
